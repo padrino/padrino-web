113adc0e0d20b36beef437980063a0dd82b4def7
Nathan Esquenazi <nesquena@gmail.com>
2026-03-28 22:10:40 -0700

    Remove export default from activateNavigationToggler function

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,4 +1,4 @@
-export default function activateNavigationToggler() {
+function activateNavigationToggler() {
   var toggler = document.getElementById('navigation__toggler');
   toggler.addEventListener('click', toggleNavigation);
 }
@@ -14,7 +14,7 @@ function toggleNavigation() {
   }
 }
 
-export default function activateGoogleAnalytics() {
+function activateGoogleAnalytics() {
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
